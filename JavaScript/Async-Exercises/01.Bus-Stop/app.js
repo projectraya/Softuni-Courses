b9c6b878dcb5cd75d148529f5379332d5f82b77f
projectraya <rminkova007@gmail.com>
2025-04-30 11:53:51 +0300

    More solved tasks

--- JavaScript/Async-Exercises/01.Bus-Stop/app.js
+++ JavaScript/Async-Exercises/01.Bus-Stop/app.js
@@ -1,3 +1,45 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    const stopId = document.getElementById("stopId").value;
+    const buses = document.getElementById("buses");
+    const stopName = document.getElementById("stopName");
+    const BASE_URL = "http://localhost:3030/jsonstore/bus/businfo/"
+    
+    buses.innerHTML = "";
+
+    try{
+        const res = await fetch(`${BASE_URL}${stopId}`);
+        const data = await res.json();
+        for (const [key, value] of Object.entries(data.buses)) {
+            const li = document.createElement("li");
+            li.textContent = `Bus ${key} arrives in ${value} minutes`;
+            buses.appendChild(li);
+        }
+        stopName.textContent = data.name;
+    }
+    catch(err) {
+        stopName.textContent = "Error";
+    }
+
+    // fetch(`${BASE_URL}${stopId}`)
+    //     .then(res => {
+    //         if (res.status !== 200) {
+    //             throw new Error("Error: " + res.status);
+    //         }
+    //         return res.json();
+
+    //     }).then(data => {
+
+    //         for (const [key, value] of Object.entries(data.buses)) {
+    //             const li = document.createElement("li");
+    //             li.textContent = `Bus ${key} arrives in ${value} minutes`;
+    //             buses.appendChild(li);
+    //         }
+    //         document.getElementById("stopName").textContent = data.name;
+            
+
+    //     }).catch(err => {
+    //         console.error(err);
+    //         document.getElementById("stopName").textContent = "Error";
+    //     });
+
 }
--- JavaScript/Async-Exercises/02.Bus-Schedule/app.js
+++ JavaScript/Async-Exercises/02.Bus-Schedule/app.js
@@ -1,11 +1,49 @@
 function solve() {
 
-    function depart() {
-        console.log('Depart TODO...');
+    const infoBox = document.getElementById('info');
+    const departButton = document.getElementById('depart');
+    const arriveButton = document.getElementById('arrive');
+
+    let stop = {
+        name: '',
+        next: 'depot'
+    }
+    async function depart() {
+        try{
+            const response = await fetch(`http://localhost:3030/jsonstore/bus/schedule/${stop.next}`);
+
+            if(!response.ok) {
+                throw new Error('Error fetching data');
+            }
+
+            const data = await response.json();
+            stop.name = data.name;
+            stop.next = data.next;
+            departButton.disabled = true;
+            arriveButton.disabled = false;
+            infoBox.textContent = `Next stop ${stop.name}`;
+        }
+        catch (error) {
+            infoBox.textContent = 'Error';
+
+            departButton.disabled = true;
+            arriveButton.disabled = true;
+            return;
+        }
+        infoBox.textContent = `Next stop ${stop.name}`;
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoBox.textContent = '';
+        infoBox.textContent = `Arriving at ${stop.name}`;
+        departButton.disabled = false;
+        arriveButton.disabled = true;
+        // stop.next = stop.name;
+        // stop.name = '';
+        // departButton.disabled = false;
+        // arriveButton.disabled = true;
+        // infoBox.textContent = `Next stop ${stop.name}`;
+
     }
 
     return {
